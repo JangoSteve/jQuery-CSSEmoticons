af29de1700d267893210efe4c24d9f7ecdab0c48
Steve Schwartz <steve@alfajango.com>
2010-09-10 03:02:25 -0400

    Actually got the plugin working with a simple array for all of the desired emoticons to be matched

--- javascripts/jquery.cssemoticons.js
+++ javascripts/jquery.cssemoticons.js
@@ -13,11 +13,42 @@
   $.fn.cssEmoticons = function(options) {
 
     var opts = $.extend({}, $.fn.cssEmoticons.defaults, options);
+    
+    var escapeCharacters = [ ")", "(" ];
+    
+    var threeCharacterEmoticons = [
+      ":-)",
+      ":-(",
+      ":'("
+    ];
+    
+    var twoCharacterEmoticons = [ // separate these out so that we can add a space between the characters for better proportions
+      ":)",
+      ":("
+    ];
+    
+    var specialRegex = new RegExp( '(\\' + escapeCharacters.join('|\\') + ')', 'g' );
+    
+    for ( var i=threeCharacterEmoticons.length-1; i>=0; --i ){
+      threeCharacterEmoticons[i] = threeCharacterEmoticons[i].replace(specialRegex,'\\$1');
+      threeCharacterEmoticons[i] = new RegExp( '(' + threeCharacterEmoticons[i] + ')', 'g' );
+    }
+    
+    for ( var i=twoCharacterEmoticons.length-1; i>=0; --i ){
+      twoCharacterEmoticons[i] = twoCharacterEmoticons[i].replace(specialRegex,'\\$1');
+      twoCharacterEmoticons[i] = new RegExp( 
+        '(' + twoCharacterEmoticons[i][0] + ')(' + twoCharacterEmoticons[i].substring(1,twoCharacterEmoticons[i].length) + ')', 'g' 
+      );
+    }
 
     return this.each(function() {
       var container = $(this);
-      var emoticon = /(:-\))/;
-      container.html(container.html().replace(emoticon,"<span class='css-emoticon'>$1</span>"));
+      $(threeCharacterEmoticons).each(function(){
+        container.html(container.html().replace(this,"<span class='css-emoticon'>$1</span>"));
+      });
+      $(twoCharacterEmoticons).each(function(){
+        container.html(container.html().replace(this,"<span class='css-emoticon'>$1 $2</span>"));
+      });
     });
   }
 
